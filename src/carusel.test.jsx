9bfcba755f8e41846bdc11158e64c9cd538088c2
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-02-21 17:27:36 +0000

    fix carusel queue + tests

--- src/carusel.test.jsx
+++ src/carusel.test.jsx
@@ -38,4 +38,38 @@ describe('<Carusel/>', () => {
             });
         });
     });
+
+    describe('internal callbacks', () => {
+        describe('::handleNewPosition', () => {
+            it('when [::limit] smaller than a distance from [::position] till end of the queue it should present elements in queue between [::position] and [::position + ::limit]', () => {
+                const items = [
+                    {
+                        src: '{{src0}}',
+                        tags: '{{tags0}}',
+                    },
+                    {
+                        src: '{{src1}}',
+                        tags: '{{tags1}}',
+                    },
+                    {
+                        src: '{{src2}}',
+                        tags: '{{tags2}}',
+                    },
+                    {
+                        src: '{{src3}}',
+                        tags: '{{tags3}}',
+                    },
+                ];
+                const c = shallow(<Carusel {...props} items={items} limit={2} position={1}/>);
+
+                expect(c.state('items')).toMatchSnapshot();
+            });
+
+            it(`when [::limit] higher than a distance from [::position] till end of the queue, it should fill queue by injecting items from start of scope`, () => {
+                const c = shallow(<Carusel {...props} limit={3} position={2}/>);
+
+                expect(c.state('items')).toMatchSnapshot();
+            });
+        });
+    });
 });
